f0df002a4a23fe05c89f8d514dc0adbba81bb03b
Sagar Kumar Rath <101728627+krishnasagar611@users.noreply.github.com>
2024-08-15 12:45:03 +0530

    adda
    add

--- components/Latestinsights/LatestInsights.tsx
+++ components/Latestinsights/LatestInsights.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import Image from "next/image"; // Assuming you'll use Image component for the image
+import Image from "next/image";
+import { ChevronDown, ChevronRight } from "lucide-react";
 
 export default function LatestInsights() {
   return (
@@ -7,75 +8,99 @@ export default function LatestInsights() {
       <h2 className="text-2xl font-bold mb-6 text-gray-900 dark:text-gray-100">
         Latest insights
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/* First Row: Three Cards */}
-        <div className="bg-gray-100 dark:bg-gray-800 p-6 flex flex-col justify-between">
-          <div>
-            <span className="inline-block bg-black text-white text-xs font-bold px-2 py-1 mb-4">
-              Tool
-            </span>
-            <h3 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-100">
-              Haiven™ team assistant: accelerate GenAI adoption across your software delivery lifecycle
-            </h3>
-          </div>
-          <Link href="/learn-more">
-            <div className="text-blue-500 hover:underline mt-4 cursor-pointer dark:text-blue-400 dark:hover:text-blue-600">
-              Learn more &rarr;
+      <div className="grid grid-cols-6 gap-6">
+        {/* First Card with Image */}
+        <div className="col-span-6 md:col-span-3 cursor-pointer transform transition-transform duration-300 hover:-translate-x-2">
+          <div className="flex-1 flex flex-col justify-between bg-white dark:bg-slate-800 py-4 px-4 h-[280px] ">
+            <div>
+              <span className="inline-block bg-black text-white text-xs font-bold px-4 py-2 mb-4 rounded-sm">
+                Tool
+              </span>
+              <h3 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-100">
+                Haiven™ team assistant: accelerate GenAI adoption across your
+                software delivery lifecycle
+              </h3>
             </div>
-          </Link>
-        </div>
-
-        <div className="bg-teal-600 dark:bg-teal-700 text-white p-6 flex flex-col justify-between">
-          <div>
-            <span className="inline-block bg-black text-white text-xs font-bold px-2 py-1 mb-4">
-              E-Book
-            </span>
-            <h3 className="text-xl font-semibold mb-4">
-              Engineering Intelligence Platform
-            </h3>
+            <Link href="/learn-more">
+              <div className="text-blue-500 hover:underline mt-4 cursor-pointer dark:text-blue-400 dark:hover:text-blue-600 font-bold">
+                Learn more <ChevronRight className="inline w-4 h-4" />
+              </div>
+            </Link>
           </div>
-          <Link href="/get-ebook">
-            <div className="text-white hover:underline mt-4 cursor-pointer">
-              Get the e-book &rarr;
-            </div>
-          </Link>
-        </div>
-
-        <div className="">
-          <Image
-            src="/images/insight_2nd_img.jpg" // Replace with your image path
-            alt="AI Adoption"
-            className="w-full h-auto"
-            width={500}
-            height={500}
-          />
-        </div>
-
-        {/* Second Row: Image and One Card */}
-        <div className="col-span-2">
-          <Image
-            src="/images/insight_lights.jpg" // Replace with your image path
-            alt="AI Adoption"
-            className="w-full h-auto"
-            width={1000}
-            height={500}
-          />
-        </div>
-
-        <div className="bg-green-500 dark:bg-green-600 text-white p-6 flex flex-col justify-between">
-          <div>
-            <span className="inline-block bg-black text-white text-xs font-bold px-2 py-1 mb-4">
-              Podcast
-            </span>
-            <h3 className="text-xl font-semibold mb-4">
-              Leveraging generative AI at Bosch
-            </h3>
+          <div className="flex-shrink-0">
+            <Image
+              src="/images/insight_lights.jpg"
+              alt="AI Adoption"
+              className="w-full h-[295px] object-cover"
+              width={200}
+              height={150}
+            />
           </div>
-          <Link href="/listen-podcast">
-            <div className="text-white hover:underline mt-4 cursor-pointer">
-              Listen to this podcast &rarr;
+        </div>
+
+        {/* Second Card */}
+        <div className="col-span-6 md:col-span-3 flex flex-col">
+          <div className="flex justify-center gap-6 mb-4">
+            <div className="space-y-4">
+              <div className="bg-teal-600 dark:bg-teal-700 text-white p-6 col-span-6 md:col-span-3 flex flex-col justify-between h-[280px] w-[285px] transform transition-transform duration-300 hover:-translate-x-2 cursor-pointer">
+                <div>
+                  <span className="inline-block bg-black text-white text-xs font-bold px-2 mb-4">
+                    E-Book
+                  </span>
+                  <h3 className="text-xl font-semibold mb-4">
+                    Engineering Intelligence Platform
+                  </h3>
+                </div>
+                <Link href="/get-ebook">
+                  <div className="text-white hover:underline mt-4 cursor-pointer">
+                    Get the e-book &rarr;
+                  </div>
+                </Link>
+              </div>
+              <div className="bg-orange-600 dark:bg-orange-700 text-white p-6 col-span-6 md:col-span-3 flex flex-col justify-between w-[285px] h-[280px] transform transition-transform duration-300 hover:-translate-x-2 cursor-pointer">
+                <div>
+                  <span className="inline-block bg-black text-white text-xs font-bold px-2 mb-4">
+                    Publication
+                  </span>
+                  <h3 className="text-xl font-semibold mb-4">
+                    Holistic cybersecurity
+                  </h3>
+                </div>
+                <Link href="/read-edition">
+                  <div className="text-white hover:underline mt-4 cursor-pointer">
+                    Read this edition &rarr;
+                  </div>
+                </Link>
+              </div>
             </div>
-          </Link>
+            <div className="col-span-6 md:col-span-3 flex flex-col transform transition-transform duration-300 hover:-translate-x-2 cursor-pointer">
+              <div className="flex-shrink-0 ">
+                <Image
+                  src="/images/insight_2nd_img.jpg"
+                  alt="AI Adoption"
+                  className="w-full h-[280px] object-cover"
+                  width={200}
+                  height={150}
+                />
+              </div>
+              <div className="flex-1 flex flex-col justify-between bg-white dark:bg-slate-800 px-4 py-4">
+                <div>
+                  <span className="inline-block bg-black text-white text-xs font-bold px-4 py-2 mb-4 rounded-sm">
+                    Tool
+                  </span>
+                  <h3 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-100">
+                    Haiven™ team assistant: accelerate GenAI adoption across
+                    your software delivery lifecycle
+                  </h3>
+                </div>
+                <Link href="/learn-more">
+                  <div className="text-blue-500 hover:underline mt-4 cursor-pointer dark:text-blue-400 dark:hover:text-blue-600 font-bold">
+                    Learn more <ChevronRight className="inline w-4 h-4" />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
